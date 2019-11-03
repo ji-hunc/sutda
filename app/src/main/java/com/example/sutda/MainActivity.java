376a73bc1ea2c711033a0c8ce782b0c5c14d9d70
JihunChoi <geehoon62017@gmail.com>
2019-11-03 21:21:01 +0900

    땡잡이 오류 수정 ● || -> &&

--- app/src/main/java/com/example/sutda/MainActivity.java
+++ app/src/main/java/com/example/sutda/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.sutda;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,13 +15,11 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Arrays;
-
 
 public class MainActivity extends AppCompatActivity {
     int sum = 0;
     int first = 1;
-    String[] nums = new String[2];
+    String[] numbers = new String[2];
     int clickCount = 0;
     ImageView firstImageButton;
     ImageView secondImageButton;
@@ -133,11 +130,11 @@ public class MainActivity extends AppCompatActivity {
                             al.setTextColor(Color.RED);
                             scrollToView(al, listScroll, 0);
                         } else if (sum == 4) {
-                            if (nums[0].equals("february1") || nums[0].equals("february2")) {
+                            if (numbers[0].equals("february1") || numbers[0].equals("february2")) {
                                 resultText.setText("2땡");
                                 scrollToView(dd2, listScroll, 0);
                                 dd2.setTextColor(Color.RED);
-                            } else if ((nums[0].equals("january1")) && nums[1].equals("march1") || (nums[1].equals("january1")) && nums[0].equals("march1")) {
+                            } else if ((numbers[0].equals("january1")) && numbers[1].equals("march1") || (numbers[1].equals("january1")) && numbers[0].equals("march1")) {
                                 resultText.setText("1·3광땡");
                                 gd13.setTextColor(Color.RED);
                                 scrollToView(gd13, listScroll, 0);
@@ -147,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg4, listScroll, 0);
                             }
                         } else if (sum == 5) {
-                            if ((nums[0].equals("january1") || nums[0].equals("january2")) || (nums[0].equals("april1") || nums[0].equals("april2"))) {
+                            if ((numbers[0].equals("january1") || numbers[0].equals("january2")) || (numbers[0].equals("april1") || numbers[0].equals("april2"))) {
                                 resultText.setText("독사");
                                 ds.setTextColor(Color.RED);
                                 scrollToView(ds, listScroll, 0);
@@ -157,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg5, listScroll, 0);
                             }
                         } else if (sum == 6) {
-                            if (nums[0].equals("march1") || nums[0].equals("march2")) {
+                            if (numbers[0].equals("march1") || numbers[0].equals("march2")) {
                                 resultText.setText("3땡");
                                 dd3.setTextColor(Color.RED);
                                 scrollToView(dd3, listScroll, 0);
@@ -171,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
                             gg7.setTextColor(Color.RED);
                             scrollToView(gg7, listScroll, 0);
                         } else if (sum == 8) {
-                            if (nums[0].equals("april1") || nums[0].equals("april2")) {
+                            if (numbers[0].equals("april1") || numbers[0].equals("april2")) {
                                 resultText.setText("4땡");
                                 dd4.setTextColor(Color.RED);
                                 scrollToView(dd4, listScroll, 0);
@@ -181,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg8, listScroll, 0);
                             }
                         } else if (sum == 9) {
-                            if ((nums[0].equals("january1") && nums[1].equals("august1")) || nums[0].equals("august1") && nums[1].equals("january1")) {
+                            if ((numbers[0].equals("january1") && numbers[1].equals("august1")) || numbers[0].equals("august1") && numbers[1].equals("january1")) {
                                 resultText.setText("1·8광땡");
                                 gd18.setTextColor(Color.RED);
                                 scrollToView(gd18, listScroll, 0);
@@ -191,19 +188,19 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(go, listScroll, 0);
                             }
                         } else if (sum == 10) {
-                            if (nums[0].equals("may1") || nums[0].equals("may2")) {
+                            if (numbers[0].equals("may1") || numbers[0].equals("may2")) {
                                 resultText.setText("5땡");
                                 dd5.setTextColor(Color.RED);
                                 scrollToView(dd5, listScroll, 0);
-                            } else if ((nums[0].equals("april1") || nums[0].equals("april2")) || (nums[0].equals("june1") || nums[0].equals("june2"))) {
+                            } else if ((numbers[0].equals("april1") || numbers[0].equals("april2")) || (numbers[0].equals("june1") || numbers[0].equals("june2"))) {
                                 resultText.setText("세륙");
                                 sl.setTextColor(Color.RED);
                                 scrollToView(sl, listScroll, 0);
-                            } else if ((nums[0].equals("january1") || nums[0].equals("january2")) || (nums[0].equals("september1") || nums[0].equals("september2"))) {
+                            } else if ((numbers[0].equals("january1") || numbers[0].equals("january2")) || (numbers[0].equals("september1") || numbers[0].equals("september2"))) {
                                 resultText.setText("구삥");
                                 gb.setTextColor(Color.RED);
                                 scrollToView(gb, listScroll, 0);
-                            } else if ((nums[0].equals("march1") && nums[1].equals("july1")) || (nums[0].equals("july1") || nums[1].equals("march1"))) {
+                            } else if ((numbers[0].equals("march1") && numbers[1].equals("july1")) || (numbers[0].equals("july1") && numbers[1].equals("march1"))) {
                                 resultText.setText("땡잡이");
                                 tg.setTextColor(Color.YELLOW);
                                 scrollToView(tg, listScroll, 0);
@@ -216,15 +213,15 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else {
                         if (sum == 11) {
-                            if ((nums[0].equals("march1") && nums[1].equals("august1")) || nums[0].equals("august1") && nums[1].equals("march1")) {
+                            if ((numbers[0].equals("march1") && numbers[1].equals("august1")) || numbers[0].equals("august1") && numbers[1].equals("march1")) {
                                 resultText.setText("3·8광땡");
                                 gd38.setTextColor(Color.RED);
                                 scrollToView(gd38, listScroll, 0);
-                            } else if ((nums[0].equals("january1") || nums[0].equals("january2")) || (nums[0].equals("october1") || nums[0].equals("october2"))) {
+                            } else if ((numbers[0].equals("january1") || numbers[0].equals("january2")) || (numbers[0].equals("october1") || numbers[0].equals("october2"))) {
                                 resultText.setText("장삥");
                                 jb.setTextColor(Color.RED);
                                 scrollToView(jb, listScroll, 0);
-                            } else if ((nums[0].equals("april1") && nums[1].equals("july1")) || nums[0].equals("july1") && nums[1].equals("april1")) {
+                            } else if ((numbers[0].equals("april1") && numbers[1].equals("july1")) || numbers[0].equals("july1") && numbers[1].equals("april1")) {
                                 resultText.setText("암행어사");
                                 ah.setTextColor(Color.YELLOW);
                                 scrollToView(ah, listScroll, 0);
@@ -234,7 +231,7 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg1, listScroll, 0);
                             }
                         } else if (sum == 12) {
-                            if (nums[0].equals("june1") || nums[0].equals("june2")) {
+                            if (numbers[0].equals("june1") || numbers[0].equals("june2")) {
                                 resultText.setText("6땡");
                                 dd6.setTextColor(Color.RED);
                                 scrollToView(dd6, listScroll, 0);
@@ -244,8 +241,8 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg2, listScroll, 0);
                             }
                         } else if (sum == 13) {
-                            if ((nums[0].equals("april1") || nums[0].equals("april2")) || (nums[0].equals("september1") || nums[0].equals("september2"))) {
-                                if ((nums[0].equals("april1") && nums[1].equals("september1")) || (nums[0].equals("september1") && nums[1].equals("april1"))) {
+                            if ((numbers[0].equals("april1") || numbers[0].equals("april2")) || (numbers[0].equals("september1") || numbers[0].equals("september2"))) {
+                                if ((numbers[0].equals("april1") && numbers[1].equals("september1")) || (numbers[0].equals("september1") && numbers[1].equals("april1"))) {
                                     resultText.setText("멍텅구리 구사");
                                     mg.setTextColor(Color.YELLOW);
                                     scrollToView(mg, listScroll, 0);
@@ -260,11 +257,11 @@ public class MainActivity extends AppCompatActivity {
                                 scrollToView(gg3, listScroll, 0);
                             }
                         } else if (sum == 14) {
-                            if (nums[0].equals("july1") || nums[0].equals("july2")) {
+                            if (numbers[0].equals("july1") || numbers[0].equals("july2")) {
                                 resultText.setText("7땡");
                                 dd7.setTextColor(Color.RED);
                                 scrollToView(dd7, listScroll, 0);
-                            } else if ((nums[0].equals("april1") || nums[0].equals("april2")) || (nums[0].equals("october1") || nums[0].equals("october2"))) {
+                            } else if ((numbers[0].equals("april1") || numbers[0].equals("april2")) || (numbers[0].equals("october1") || numbers[0].equals("october2"))) {
                                 resultText.setText("장사");
                                 js.setTextColor(Color.RED);
                                 scrollToView(js, listScroll, 0);
@@ -278,7 +275,7 @@ public class MainActivity extends AppCompatActivity {
                             gg5.setTextColor(Color.RED);
                             scrollToView(gg5, listScroll, 0);
                         } else if (sum == 16) {
-                            if (nums[0].equals("august1") || nums[0].equals("august2")) {
+                            if (numbers[0].equals("august1") || numbers[0].equals("august2")) {
                                 resultText.setText("8땡");
                                 dd8.setTextColor(Color.RED);
                                 scrollToView(dd8, listScroll, 0);
@@ -292,7 +289,7 @@ public class MainActivity extends AppCompatActivity {
                             gg7.setTextColor(Color.RED);
                             scrollToView(gg7, listScroll, 0);
                         } else if (sum == 18) {
-                            if (nums[0].equals("september1") || nums[0].equals("september2")) {
+                            if (numbers[0].equals("september1") || numbers[0].equals("september2")) {
                                 resultText.setText("9땡");
                                 dd9.setTextColor(Color.RED);
                                 scrollToView(dd9, listScroll, 0);
@@ -318,8 +315,8 @@ public class MainActivity extends AppCompatActivity {
 
                     sum = 0;
                     first = 1;
-                    nums[0] = " ";
-                    nums[1] = " ";
+                    numbers[0] = " ";
+                    numbers[1] = " ";
                     clickCount = 0;
                 }
 
@@ -333,8 +330,8 @@ public class MainActivity extends AppCompatActivity {
 
                     sum = 0;
                     first = 1;
-                    nums[0] = " ";
-                    nums[1] = " ";
+                    numbers[0] = " ";
+                    numbers[1] = " ";
                     clickCount = 0;
                 }
 
@@ -365,7 +362,7 @@ public class MainActivity extends AppCompatActivity {
                     firstImageButton.setAlpha(255);
                     clickCount -= 1;
                     sum -= firstScore;
-                    nums[0] = " ";
+                    numbers[0] = " ";
                     firstImageButton.setEnabled(true);
                     firstSelected.setEnabled(false);
                     first = 1;  // like first time
@@ -375,7 +372,7 @@ public class MainActivity extends AppCompatActivity {
                     firstImageButton.setAlpha(255);
                     clickCount -= 1;
                     sum -= firstScore;
-                    nums[0] = " ";
+                    numbers[0] = " ";
                     firstImageButton.setEnabled(true);
                     firstSelected.setEnabled(false);
                     first = 1; // like second time
@@ -390,7 +387,7 @@ public class MainActivity extends AppCompatActivity {
                     secondImageButton.setAlpha(255);
                     clickCount -= 1;
                     sum -= secondScore;
-                    nums[1] = " ";
+                    numbers[1] = " ";
                     secondImageButton.setEnabled(true);
                     secondSelected.setEnabled(false);
                     first = 1; // like first time
@@ -400,7 +397,7 @@ public class MainActivity extends AppCompatActivity {
                     secondImageButton.setAlpha(255);
                     clickCount -= 1;
                     sum -= secondScore;
-                    nums[1] = " ";
+                    numbers[1] = " ";
                     secondImageButton.setEnabled(true);
                     secondSelected.setEnabled(false);
                     first = 0; // like second time
@@ -424,7 +421,7 @@ public class MainActivity extends AppCompatActivity {
                 firstSelect.setImageDrawable(image);
                 firstImageButton = months;
                 firstScore = score;
-                nums[0] = month;
+                numbers[0] = month;
                 first = 0;
                 firstSelect.setEnabled(true);
             }
@@ -432,7 +429,7 @@ public class MainActivity extends AppCompatActivity {
                 secondSelect.setImageDrawable(image);
                 secondImageButton = months;
                 secondScore = score;
-                nums[1] = month;
+                numbers[1] = month;
                 secondSelect.setEnabled(true);
             }
         }
